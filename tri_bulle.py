5571c72876a1f9cd80daec5dc0ea16dfe2edb5bd
Marion Ory <marionory@laplateforme.io>
2026-03-31 11:55:27 +0200

    algo tri peigne +bulle class +methode ok

---
+++ tri_bulle.py
@@ -0,0 +1,32 @@
+class TriBulle:
+    def __init__(self, donnees):
+        self.liste = donnees.copy()
+        self.nom = "Tri Bulle"
+
+    def trier(self):
+        permutation = True
+        passage = 0
+        while permutation == True:  # je force le premier tour
+            permutation = False  # je pars du principe que la liste est en desordre tant que la permutation n a pas prouvé le contraire
+            passage = (
+                passage + 1
+            )  # a chaque tour complet la bulle est à sa place definitive
+            for i in range(
+                0, len(self.liste) - passage
+            ):  # on reduit la zone à chaque passage
+                if self.liste[i] > self.liste[i + 1]:  # on compare les voisins
+                    permutation = True
+                    self.liste[i], self.liste[i + 1] = (
+                        self.liste[i + 1],
+                        self.liste[i],
+                    )  # on switch
+
+        return self.liste
+
+
+# Le tri à bulle on compare deux valeurs voisine et on les switch
+# je pars du principe que ma loiste à trier est fausse
+# pour trier dans ma liste je la parcours et je fais un passage à chaque tour
+# des que je trouve des valeurs à permutter je les switch par ordre croissant ou decroissant par ex
+# est ce que c est bien ça?
+# je ne suis pas sur d avoir bien compris toute les lignes de codes
--- tri_peigne.py
+++ tri_peigne.py
@@ -1,5 +1,47 @@
-class TriPeigne :
-  def __init__():
+import math  # floor arrondi un nombre à l'entier inférieur le plus proche
 
 
+class TriPeigne:
+    def __init__(self, donnees):
+        self.liste = donnees.copy()
+        self.nom = "Tri à peigne"
 
+    def trier(self):
+        permutation = True
+        gap = len(self.liste)
+
+        # on boucle tant que l'ecart est grand et qu'on fait un echange
+
+        while (permutation == True) and (
+            gap > 1
+        ):  # dès que gap==1 algo finit par tribulle
+            permutation = False  # je pars du principe que la liste triees est fausse
+            gap = math.floor(gap / 1.3)  # on reduit l'ecart avec le facteur 1.3
+            if gap < 1:
+                gap = 1  # gap minimum de 1 pour comparer des données voisines
+            for i in range(0, len(self.liste) - gap):  # pour ne pas sortir de la liste
+
+                if (
+                    self.liste[i] > self.liste[i + gap]
+                ):  # on compare la 1ère et dernière clés
+                    permutation = (
+                        True  # je trouve des donnees à permutter donc je les échanges
+                    )
+                    self.liste[i], self.liste[i + gap] = (
+                        self.liste[i + gap],
+                        self.liste[i],
+                    )  # on change les positions : A ,B = B, A
+        return self.liste
+
+
+# on etablit la classe avec self et les donnnees qu on va trier
+# je crée un copy des données sinon mais futurs algo à testé seront erronés
+# en suite je passe au tri à peigne
+# pour ça j active la permutation les element de ma liste vont pouvoir passé d un coté de l autre
+# j établi que mon gap est egal à la longueur de ma liste pour commencer avant de reduire à 1.3
+# Je boucle tant que l ecart est grand et que je fait des echanges et tant que mon gap est superieur à 1
+# et que mon gap = a mon gap arrondi /1.3
+# je continu de comparer tant que mon gap n est pas à 1
+# pour i dans la longueur de ma liste moins mon ecart
+# si i dans ma liste  et i+ecart = i+ecart et i de ma liste la j inverse les positions
+# et enfin je retourne ma liste
